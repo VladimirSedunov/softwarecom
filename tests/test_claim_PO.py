cc7746ec9b0ec5167a8d40b8001a244ecb96724d
VladimirSedunov <Vladimir.Sedunov@softwarecom.ru>
2023-03-14 14:15:40 +0700

    test_claim_PO.py

--- tests/test_claim_PO.py
+++ tests/test_claim_PO.py
@@ -6,7 +6,7 @@ from tests.test_claim_dir.model.pages.form_claim import Claim, Form_Claim
 from tests.test_claim_dir.model.pages.form_main import Form_Main
 
 list_claim = [
-    Claim('Позитивный сценарий', 'Тут Пишем ФИО', 'this_is@email.mail', 'Это_телефон', 'Это сообщение', True, False, 'Ваш запрос отправлен менеджеру!'),
+    # Claim('Позитивный сценарий', 'Тут Пишем ФИО', 'this_is@email.mail', 'Это_телефон', 'Это сообщение', True, False, 'Ваш запрос отправлен менеджеру!'),
     Claim('Нет ФИО', '', 'this_is@email.mail', 'Это_телефон', 'Это сообщение', True, True, 'Укажите Ваше Ф.И.О!'),
     Claim('Нет телефона', 'Тут Пишем ФИО', 'this_is@email.mail', '', 'Это сообщение', False, True, 'Укажите Ваш контактный телефон!'),
     Claim('Нет email', 'Тут Пишем ФИО', '', 'Это_телефон', 'Это сообщение', True, True, 'Укажите правильный e-mail!'),
